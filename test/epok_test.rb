cf5162b398b19379e0548498a77f31e5fa7cda4e
Ariel Rzezak <arzezak@gmail.com>
2026-09-15 11:01:08 -0300

    Test top-level search and geocoder entry points

--- test/epok_test.rb
+++ test/epok_test.rb
@@ -1,15 +1,20 @@
 require "test_helper"
 
 class EpokTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil Epok::VERSION
+  def test_that_search_builds_a_search
+    search = Epok.search("garicoits")
+
+    assert_instance_of Epok::Search, search
+    assert_equal "garicoits", search.query
   end
 
-  def test_responds_to_search
-    assert_respond_to Epok, :search
-  end
+  def test_that_geocoder_builds_a_geocoder
+    obelisco = Epok::Location.new(x: -58.381570, y: -34.603738)
+    geocoder = Epok.geocoder(obelisco, "farmacias")
 
-  def test_responds_to_geocoder
-    assert_respond_to Epok, :geocoder
+    assert_instance_of Epok::Geocoder, geocoder
+    assert_equal(-58.381570, geocoder.x)
+    assert_equal(-34.603738, geocoder.y)
+    assert_equal "farmacias", geocoder.categories
   end
 end
